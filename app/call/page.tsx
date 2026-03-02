ed15b0fd2665f3ec045a019e1ab3b08c62fef1f7
Joseph K <joseph101kt@gmail.com>
2026-03-02 14:55:21 +0530

    moved the call from index page to a seperate /call page

---
+++ app/call/page.tsx
@@ -0,0 +1,10 @@
+import VideoStreamingComponent from "@/features/video_streaming/video_stream";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <VideoStreamingComponent />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import VideoStreamingComponent from "@/features/video_streaming/video_stream";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <VideoStreamingComponent />
+      <div className="p-10"> 
+      <h1>PREPARE</h1>
+      <h2>Pre-hospital Risk Evaluation & Patient Arrival Readiness Engine</h2>
+      <a href="/call" className="p-10">Call</a>
+      <a href="/dashboard" className="p-10">Dashboard</a>
+      </div>
     </div>
   );
 }
