ee119ee66cbef28f723ea446148e26fff1399536
sbmswebby <sbmswebby@gmail.com>
2026-03-02 08:30:21 +0530

    fixed erro create by types.zod.ts

--- types/types.zod.ts
+++ types/types.zod.ts
@@ -5,8 +5,9 @@
  */
 
 import { z } from "zod";
+type JsonValue = string | number | boolean | null | { [key: string]: JsonValue | undefined } | JsonValue[];
 
-export const jsonSchema = z.lazy(() =>
+export const jsonSchema: z.ZodType<JsonValue> = z.lazy(() =>
   z
     .union([
       z.string(),
